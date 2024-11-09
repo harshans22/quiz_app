e3a5ef581a7aab86920b7c9167fb3420c8eae1fb
harshans22 <harshhub.414@gmail.com>
2024-11-09 23:52:04 +0530

    first changes

---
+++ quize_webapp/lib/domain/widgets/home_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Stack(
+        children: [
+          SizedBox.expand(
+            child: FittedBox(
+              fit: BoxFit.cover, 
+              child: Image.asset('assets/images/home_screen.jpg'),
+            ),
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => LoginScreen()), 
+                // );
+              },
+              child: const Text('Login to the quiz'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
